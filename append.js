7b7777b795b42aafc1cc090830abd5754b1d6553
maftun siam <maftunhasans@gmail.com>
2023-02-22 00:54:10 +0600

    append

---
+++ append.js
@@ -0,0 +1,5 @@
+console.log('append js start check');
+const placesList = document.getElementById('places-list');
+const li = document.createElement('li');
+li.innerText = 'maju';
+placesList.appendChild(li);
